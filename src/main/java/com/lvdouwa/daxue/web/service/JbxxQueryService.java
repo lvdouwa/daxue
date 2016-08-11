cae94cbfd27e0295236d96673dbae2cabe9f3dd0
woshiyuefeng <darkteample@sina.com>
2016-08-11 17:24:55 +0800

    for test

---
+++ src/main/java/com/lvdouwa/daxue/web/service/JbxxQueryService.java
@@ -0,0 +1,9 @@
+package com.lvdouwa.daxue.web.service;
+
+import com.lvdouwa.daxue.core.generic.GenericService;
+import com.lvdouwa.daxue.web.model.Jbxx;
+/**
+ * Created by DYF-THINK on 2016/8/11.
+ */
+public interface JbxxQueryService extends GenericService<Jbxx,Integer> {
+}
