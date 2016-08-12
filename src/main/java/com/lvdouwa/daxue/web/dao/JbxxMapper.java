1c940960a1d6f34f627c3d6af94e8013bda09f3d
woshiyuefeng <darkteample@sina.com>
2016-08-12 10:15:37 +0800

    add new method queryByJbxx to JbxxMapper.xml

--- src/main/java/com/lvdouwa/daxue/web/dao/JbxxMapper.java
+++ src/main/java/com/lvdouwa/daxue/web/dao/JbxxMapper.java
@@ -2,16 +2,36 @@ package com.lvdouwa.daxue.web.dao;
 
 import com.lvdouwa.daxue.core.generic.GenericDao;
 import com.lvdouwa.daxue.web.model.Jbxx;
+import com.lvdouwa.daxue.web.model.JbxxExample;
+
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface JbxxMapper extends GenericDao {
+    int countByExample(JbxxExample example);
+
+    int deleteByExample(JbxxExample example);
+
     int deleteByPrimaryKey(Integer uid);
 
     int insert(Jbxx record);
 
+    int insertSelective(Jbxx record);
+
+    List<Jbxx> selectByExample(JbxxExample example);
+
     Jbxx selectByPrimaryKey(Integer uid);
 
-    List<Jbxx> selectAll();
+    int updateByExampleSelective(@Param("record") Jbxx record, @Param("example") JbxxExample example);
+
+    int updateByExample(@Param("record") Jbxx record, @Param("example") JbxxExample example);
+
+    int updateByPrimaryKeySelective(Jbxx record);
 
     int updateByPrimaryKey(Jbxx record);
+
+    ArrayList<Jbxx> queryByJbxx(Jbxx record);
+
+
 }
